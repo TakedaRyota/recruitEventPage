adf5a3da564e1acad0fea23492d5e2e9056d1009
TakedaRyota <tkdryt05@gmail.com>
2022-08-28 00:17:21 +0900

    add: ログイン機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
+
+  before_action :logged_in_user, only:[:edit, :update, :destroy]
+  
   def index
     @events = MstEvent.all
     @users = MstUser.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root "events#index"
-  get "/events", to: "events#index"
+  get    "/login",      to: "sessions#new"
+  post   "/login",      to: "sessions#create"
+  delete "/logout",     to: "sessions#destroy"
+  root   "events#index"
+  get    "/events",     to: "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
